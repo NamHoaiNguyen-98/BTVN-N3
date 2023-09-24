6e6d34064ae3b50cdf1b407bebf180ba2a63f1bd
chien2002 <chienad2002ad@gmail.com>
2023-09-24 20:16:05 +0700

    md ft

--- src/main/java/com/example/btvnn3/model/Filter.java
+++ src/main/java/com/example/btvnn3/model/Filter.java
@@ -1,2 +1,51 @@
-package com.example.btvnn3.model;public class Search {
+package com.example.btvnn3.model;
+
+public class Filter {
+    private String name ;
+    private String status;
+    private String subject ;
+    private String sex ;
+
+    public Filter(String name, String status, String subject, String sex) {
+        this.name = name;
+        this.status = status;
+        this.subject = subject;
+        this.sex = sex;
+    }
+
+    public Filter() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
