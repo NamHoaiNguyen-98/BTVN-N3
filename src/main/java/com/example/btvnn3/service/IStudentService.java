4422bc1a8cce572d9d600f955ec15b391477b362
VietAnhNguyen9 <vietanh260396@gmail.com>
2023-09-24 16:04:47 +0700

    chuyen sang iStudent

--- src/main/java/com/example/btvnn3/service/IStudentService.java
+++ src/main/java/com/example/btvnn3/service/IStudentService.java
@@ -1,6 +1,13 @@
 package com.example.btvnn3.service;
 
 import com.example.btvnn3.model.Student;
+import org.springframework.data.domain.Page;
+
+import java.awt.print.Pageable;
 
 public interface IStudentService extends IGenerateService<Student> {
+    Page<Student> searchByName(String name, Pageable pageable);
+    Page<Student> searchByStatus(Long id, Pageable pageable);
+    Page<Student> searchBySubject(Long id, Pageable pageable);
+    Page<Student> searchByAddress(String address, Pageable pageable);
 }
