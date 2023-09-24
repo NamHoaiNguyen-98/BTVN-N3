ed49f29a456f89ff959575d636784471302f71eb
thanhtr297 <thanh.tranminh.hanoi@gmail.com>
2023-09-24 13:27:25 +0700

    thanh subject

--- src/main/java/com/example/btvnn3/service/impl/SubjectService.java
+++ src/main/java/com/example/btvnn3/service/impl/SubjectService.java
@@ -1,34 +1,40 @@
 package com.example.btvnn3.service.impl;
 
 import com.example.btvnn3.model.Subject;
+import com.example.btvnn3.repository.ISubjectRepository;
 import com.example.btvnn3.service.ISubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 public class SubjectService implements ISubjectService {
+
+    @Autowired
+    private ISubjectRepository subjectRepository;
     @Override
     public Iterable<Subject> findAll() {
-        return null;
+        return subjectRepository.findAll();
     }
 
     @Override
     public Optional<Subject> findOne(Long id) {
-        return Optional.empty();
+        return subjectRepository.findById(id);
     }
 
     @Override
     public void create(Subject subject) {
-
+        subjectRepository.save(subject);
     }
 
     @Override
     public Subject update(Subject subject) {
 
-        return subject;
+        return subjectRepository.save(subject);
     }
 
     @Override
     public void delete(Long id) {
-
+        subjectRepository.deleteById(id);
     }
 }
